9beaf5967e4523961a0a5a1fa8f21478d4e8fb39
Hasim <shasim148@gmail.com>
2024-04-16 21:42:08 +0530

    some design and suggestion added for symbol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatAutocompleteModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/currency-converter/currency-converter.component.ts
+++ src/app/currency-converter/currency-converter.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CurrencyService } from '../currency.service';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-currency-converter',
@@ -12,10 +14,125 @@ export class CurrencyConverterComponent {
   symbols: string = '';
   conversionResult: any;
   errorMessage: string = '';
+  Maxdate: any = '';
+  // Example list of suggested symbols
+  suggestedSymbols: string[] = [
+    'USD', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD',
+    'CHF', 'CNY', 'SEK', 'NZD', 'MXN', 'SGD',
+    'HKD', 'NOK', 'KRW', 'TRY', 'INR', 'RUB',
+    'BRL', 'ZAR', 'DKK', 'PLN', 'THB', 'IDR',
+    'HUF', 'CZK', 'ILS', 'CLP', 'PHP', 'AED',
+    'SAR', 'RON', 'MYR', 'COP', 'ARS', 'TWD',
+    'VND', 'NGN', 'UAH', 'EGP', 'PKR', 'IQD',
+    'BDT', 'KES', 'KWD', 'CLF', 'IRR', 'DZD',
+    'OMR', 'QAR', 'MAD', 'CUC', 'PYG', 'GTQ',
+    'HRK', 'XAF', 'ETB', 'RSD', 'SDG', 'VEF',
+    'BOB', 'TZS', 'TND', 'AFN', 'LBP', 'XOF',
+    'UGX', 'YER', 'XAU', 'NPR', 'CRC', 'BHD',
+    'BND', 'BYN', 'ISK', 'JMD', 'MUR', 'MKD',
+    'LKR', 'UAH', 'GEL', 'ALL', 'GHS', 'DOP',
+    'XCD', 'UZS', 'TMT', 'SZL', 'AOA', 'AZN',
+    'MZN', 'TJS', 'BAM', 'HTG', 'BWP', 'FJD',
+    'XPF', 'SCR', 'GNF', 'ZMW', 'LAK', 'MNT',
+    'MOP', 'MVR', 'BIF', 'SLL', 'KYD', 'NAD',
+    'MWK', 'CDF', 'ERN', 'TOP', 'DJF', 'GNF',
+    'BSD', 'BZD', 'SRD', 'ANG', 'SBD', 'VUV',
+    'WST', 'STD', 'FKP', 'CUP', 'KGS', 'LSL',
+    'LRD', 'MGA', 'MDL', 'MRO', 'KMF', 'XPF',
+    'SZL', 'TJS', 'SVC', 'GNF', 'XAF', 'XOF',
+    'YER', 'SRD', 'TRY', 'ZWL', 'SOS', 'HTG',
+    'PGK', 'QAR', 'OMR', 'NIO', 'LYD', 'LKR',
+    'KHR', 'KPW', 'JOD', 'GYD', 'GIP', 'FKP',
+    'ERN', 'ETB', 'CVE', 'BMD', 'BIF', 'ANG',
+    'AMD', 'MAD', 'DJF', 'KYD', 'BSD', 'AWG',
+    'XCD', 'MRO', 'DZD', 'SYP', 'SLL', 'RWF',
+    'KPW', 'VUV', 'UZS', 'TMT', 'KHR', 'PGK',
+    'MMK', 'KGS', 'KMF', 'FJD', 'CVE', 'BTN',
+    'BIF', 'SYP', 'STD', 'RWF', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG',
+    'SVC', 'ERN', 'CVE', 'SYP', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG',
+    'SVC', 'ERN', 'CVE', 'SYP', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG',
+    'SVC', 'ERN', 'CVE', 'SYP', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG',
+    'SVC', 'ERN', 'CVE', 'SYP', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG',
+    'SVC', 'ERN', 'CVE', 'SYP', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG',
+    'SVC', 'ERN', 'CVE', 'SYP', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG',
+    'SVC', 'ERN', 'CVE', 'SYP', 'KPW', 'XPF',
+    'VUV', 'TOP', 'SLL', 'RSD', 'PGK', 'MMK',
+    'KGS', 'KMF', 'BTN', 'ANG', 'SVC', 'ERN',
+    'CVE', 'SYP', 'KPW', 'XPF', 'VUV', 'TOP',
+    'SLL', 'RSD', 'PGK', 'MMK', 'KGS', 'KMF',
+    'BTN', 'ANG', 'SVC', 'ERN', 'CVE', 'SYP',
+    'KPW', 'XPF', 'VUV', 'TOP', 'SLL', 'RSD',
+    'PGK', 'MMK', 'KGS', 'KMF', 'BTN', 'ANG'];
 
-  constructor(private CurrencyService: CurrencyService) { }
+  // Autocomplete functionality
+  symbolControl = new FormControl();
+  filteredSymbols: Observable<string[]>;
 
+
+  constructor(private CurrencyService: CurrencyService) {
+    this.filteredSymbols = this.symbolControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(symbol => symbol ? this.filterSymbols(symbol) : this.suggestedSymbols.slice())
+      );
+  }
   ngOnInit(): void {
+    this.Maxdate = new Date();
+
+  }
+
+  filterSymbols(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.suggestedSymbols.filter(symbol => symbol.toLowerCase().includes(filterValue));
   }
 
   convertCurrency(): void {
@@ -27,11 +144,17 @@ export class CurrencyConverterComponent {
     this.CurrencyService.convertCurrency(this.date, this.baseCurrency, this.symbols.split(','))
       .subscribe(response => {
         this.conversionResult = response;
-        console.log(this.conversionResult);
         this.errorMessage = '';
       }, error => {
         console.error(error);
         this.errorMessage = 'An error occurred while fetching conversion data';
       });
+  }
+  getMaxDate(): string {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, '0');
+    const day = today.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 }
